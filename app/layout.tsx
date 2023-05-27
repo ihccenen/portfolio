94ed62ae9cf9a388cb6731daf16e189d6413cef1
ihccenen <wirkz01@gmail.com>
2023-05-27 01:15:28 -0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500'] });
+
+export const metadata = {
+  title: 'Portfolio',
+  description: 'Portfolio',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
